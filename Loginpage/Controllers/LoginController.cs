ca8c1d44896ca87329a5829d64f87e0b4ba484be
kannan <kannanbsk1609080@gmail.com>
2022-10-29 14:48:59 +0530

    Add project files.

---
+++ Loginpage/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loginpage.Models;
+
+namespace Loginpage.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<UserModel> PutValue()
+        {
+            var users = new List<UserModel>
+            {
+                new UserModel{id=1,username="12345",password="kannan"},
+                new UserModel{id=2,username="13579",password="kannan123"},
+                new UserModel{id=3,username="24680",password="kannan2000"},
+                new UserModel{id=4,username="98765",password="kannanabc"}
+            };
+
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(UserModel usr)
+        {
+            var u = PutValue();
+
+            var ue = u.Where(u => u.username.Equals(usr.username));
+
+            var up = ue.Where(p => p.password.Equals(usr.password));
+
+            if(up.Count()==1)
+            {
+                ViewBag.message = "Login Success";
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Login");
+            }
+        }
+    }
+}
